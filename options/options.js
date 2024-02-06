6055037ab4cb4e146601d326e6577bd356cdd764
Crowdbotics <team@crowdbotics.com>
2024-02-06 15:09:55 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wispy-mountain-46034",
+  url: "https://wispy-mountain-46034.botics.co",
+  api: "https://wispy-mountain-46034.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
